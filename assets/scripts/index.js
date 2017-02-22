eb72a32c610205148aee2d3c20b129817d592043
yourUsername <your_email@example.com>
2017-02-22 15:10:44 -0500

    Feature branch merge

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,19 +6,9 @@ require('./example');
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
-<<<<<<< HEAD
-const authEvents = require('./logic/events.js');
-
-
-$(() => {
-  authEvents.addHandlers();
-});
-
-=======
 const authEvents = require('./auth/events.js');
 
 
->>>>>>> feature
 $(() => {
   authEvents.addHandlers();
 });
