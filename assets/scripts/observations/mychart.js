f80a4639588e62d0493d8c58e368a22f6c5489c7
Stephen Wisner <spwisner@gmail.com>
2017-03-21 15:00:34 -0400

    Chart Attempt 1 Completed

--- assets/scripts/observations/mychart.js
+++ assets/scripts/observations/mychart.js
@@ -1,47 +1,68 @@
 'use strict';
 
-let testReplace = [12, 11, 3, 5, 2, 3];
+let etData = [12, 7];
 
-let aetChart = document.getElementById("aet-chart");
+let oftData = [5, 7, 12];
 
-const createChart = function(results, element) {
+let etChart = document.getElementById("et-chart");
+let oftChart = document.getElementById("oft-chart");
+
+const createChart = function(labeltype, results, element, bgc, brd) {
   let myChart = new Chart(element, {
-      type: 'bar',
-      data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: 'Number Observed',
-              data: results,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
+    type: 'bar',
+    data: {
+      labels: labeltype,
+      datasets: [{
+        label: 'Number Observed',
+        data: results,
+        backgroundColor: bgc,
+        borderColor: brd,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
           }
+        }]
       }
+    }
   });
   return myChart;
 };
 
-createChart(testReplace, aetChart);
+const chartGenerator = function(etData, oftData) {
+
+  let etLabelType = ["Target", "Comparison"];
+  let oftLabeltype = ["Oft V", "Oft P", "Oft M"];
+
+  let bgColorEt = [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)'
+  ];
+
+  let brdColorEt = [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)'
+  ];
+
+  let bgColorOft = [
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)'
+  ];
+
+  let brdColorOft = [
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)'
+  ];
+
+  createChart(etLabelType, etData, etChart, bgColorEt, brdColorEt);
+  createChart(oftLabeltype, oftData, oftChart, bgColorOft, brdColorOft);
+
+};
+
+chartGenerator(etData, oftData);
