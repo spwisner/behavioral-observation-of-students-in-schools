08d73c42a2b102b7e9d437a51bec4ef87a663695
Stephen Wisner <spwisner@gmail.com>
2017-03-22 22:09:25 -0400

    Completed Comparison Chart

--- assets/scripts/chart/mychart.js
+++ assets/scripts/chart/mychart.js
@@ -1,98 +1,143 @@
 'use strict';
 
 const store = require('../store');
+const Chart = require('chart.js');
 
-let chartDataObject = store.chartData;
+// let chartDataObject = store.chartData;
 
-const createChart = function(data) {
+
+
+
+const createChart = function(targetFinalArr, randomFinalArr) {
   const etChart = document.getElementById("et-chart").getContext('2d');
   const oftChart = document.getElementById("oft-chart").getContext('2d');
-  let etArr = [data[0], data[1]];
-  let oftArr = [data[2], data[3], data[4]];
 
-  const buildChart = function(arr) {
-    let dataArr;
-    let label;
-    let chartType;
-    let etArr = [data[0], data[1]];
-    let oftArr = [data[2], data[3], data[4]];
+  const buildChart = function() {
+    let targetEtArr = [targetFinalArr[0], targetFinalArr[1]];
+    let targetOftArr = [targetFinalArr[2], targetFinalArr[3], targetFinalArr[4]];
+    let randomEtArr = [randomFinalArr[0], randomFinalArr[1]];
+    let randomOftArr = [randomFinalArr[2], randomFinalArr[3], randomFinalArr[4]];
     let etLabels = ["AET", "PET"];
     let oftLabels = ["Oft-V", "Oft-M", "Oft-P"];
 
-    if (arr.length === 3) {
-      dataArr = oftArr;
-      label = oftLabels;
-      chartType = oftChart;
-    } else {
-      dataArr = etArr;
-      label = etLabels;
-      chartType = etChart;
-    }
-
-    let myChart = new Chart(chartType, {
+    let etChartDisplay = new Chart(etChart, {
       type: 'bar',
       data: {
-        labels: label,
+        labels: etLabels,
         datasets: [{
           label: 'Target',
-          data: dataArr,
+          data: targetEtArr,
           backgroundColor: "rgba(153,255,51,1)"
         }, {
           label: 'Random',
-          data: dataArr,
+          data: randomEtArr,
           backgroundColor: "rgba(255,153,0,1)"
         }]
       }
     });
+
+    let oftChartDisplay = new Chart(oftChart, {
+      type: 'bar',
+      data: {
+        labels: oftLabels,
+        datasets: [{
+          label: 'Target',
+          data: targetOftArr,
+          backgroundColor: "rgba(153,255,51,1)"
+        }, {
+          label: 'Random',
+          data: randomOftArr,
+          backgroundColor: "rgba(255,153,0,1)"
+        }]
+      }
+    });
+
   };
-  buildChart(etArr);
-  buildChart(oftArr);
+  buildChart();
 };
 
 const getColumnSums = function(data) {
-  let finalArr = [];
-  let aetDataCount = 0;
-  let petDataCount = 0;
-  let oftVDataCount = 0;
-  let oftMDataCount = 0;
-  let oftPDataCount = 0;
+  // target array and count vars
+  let targetFinalArr = [];
+  let targetAetDataCount = 0;
+  let targetPetDataCount = 0;
+  let targetOftVDataCount = 0;
+  let targetOftMDataCount = 0;
+  let targetOftPDataCount = 0;
+
+  // target array and count vars
+  let randomFinalArr = [];
+  let randomAetDataCount = 0;
+  let randomPetDataCount = 0;
+  let randomOftVDataCount = 0;
+  let randomOftMDataCount = 0;
+  let randomOftPDataCount = 0;
 
   let dataArr = data.observations;
 
   for (let i = 0; i < dataArr.length; i++) {
+
     let aetData = dataArr[i].aet;
     if (aetData === true) {
-      aetDataCount += 1;
+      if ( (i+1) % 5 === 0 ) {
+        randomAetDataCount += 1;
+      } else {
+        targetAetDataCount += 1;
+      }
     }
 
     let petData = dataArr[i].pet;
     if (petData === true) {
-      petDataCount += 1;
+      if ( (i+1) % 5 === 0 ) {
+        randomPetDataCount += 1;
+      } else {
+        targetPetDataCount += 1;
+      }
     }
+
 
     let oftVData = dataArr[i].oft_v;
     if (oftVData === true) {
-      oftVDataCount += 1;
+      if ( (i+1) % 5 === 0 ) {
+        randomOftVDataCount += 1;
+      } else {
+        targetOftVDataCount += 1;
+      }
     }
+
 
     let oftMData = dataArr[i].oft_m;
     if (oftMData === true) {
-      oftMDataCount += 1;
+      if ( (i+1) % 5 === 0 ) {
+        randomOftMDataCount += 1;
+      } else {
+        targetOftMDataCount += 1;
+      }
     }
 
     let oftPData = dataArr[i].oft_p;
     if (oftPData === true) {
-      oftPDataCount += 1;
+      if ( (i+1) % 5 === 0 ) {
+        randomOftPDataCount += 1;
+      } else {
+        targetOftPDataCount += 1;
+      }
     }
   }
 
-  finalArr.push(aetDataCount);
-  finalArr.push(petDataCount);
-  finalArr.push(oftVDataCount);
-  finalArr.push(oftMDataCount);
-  finalArr.push(oftPDataCount);
+  targetFinalArr.push(targetAetDataCount);
+  targetFinalArr.push(targetPetDataCount);
+  targetFinalArr.push(targetOftVDataCount);
+  targetFinalArr.push(targetOftMDataCount);
+  targetFinalArr.push(targetOftPDataCount);
 
-  createChart(finalArr);
+  randomFinalArr.push(randomAetDataCount);
+  randomFinalArr.push(randomPetDataCount);
+  randomFinalArr.push(randomOftVDataCount);
+  randomFinalArr.push(randomOftMDataCount);
+  randomFinalArr.push(randomOftPDataCount);
+
+  createChart(targetFinalArr, randomFinalArr);
 };
 
 
